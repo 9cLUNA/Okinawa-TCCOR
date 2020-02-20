297f335a59f50b5cc4b299aab0de5235ac76d621
9cLUNA <christopher.alan.luna+github@gmail.com>
2020-02-20 15:03:21 +0900

    Added handling of exception with try block.

--- tccor.py
+++ tccor.py
@@ -32,41 +32,51 @@ url = 'http://www.kadena.af.mil/Agencies/Local-Weather'
 # begin soup to read the webpage and parse as html
 soup = BeautifulSoup(urlopen(url).read(), "html.parser")
 
-# looking for a section titled accordingly
-# then looking for the image directly after it 
-tccor = soup.find(text="Current TCCOR Information").findNext('img')['src']
+try:
 
-# creating url link to the image file we found
-# first we get the url parent
-# then combine url with the img src to get complete url
-urllink = 'https://www.kadena.af.mil'
-tccorurl = urllink + tccor
+    # looking for a section titled accordingly
+    # then looking for the image directly after it 
+    tccor = soup.find(text="Current TCCOR Information").findNext('img')['src']
 
-tccorimg = requests.get(tccorurl, stream=True)
-with open(tmp_img, 'wb') as out_file:
-    shutil.copyfileobj(tccorimg.raw, out_file)
+    # creating url link to the image file we found
+    # first we get the url parent
+    # then combine url with the img src to get complete url
+    urllink = 'https://www.kadena.af.mil'
+    tccorurl = urllink + tccor
 
-image = Image.open(tmp_img).convert('RGB')
-# image might be gif and JPG doesn't support transparency
-#image = image.convert('RGB')
+    tccorimg = requests.get(tccorurl, stream=True)
+    with open(tmp_img, 'wb') as out_file:
+        shutil.copyfileobj(tccorimg.raw, out_file)
 
-# enlarge img for better accuracy
-basewidth = 4000
-wpercent = (basewidth/float(image.size[0]))
-hsize = int((float(image.size[1])*float(wpercent)))
-image = image.resize((basewidth,hsize), Image.ANTIALIAS)
-# ensure saving image with 300 dpi
-image.save(tmp_img, dpi=(400,400))
-# pytesseract work
-tccordata = pt.image_to_string(tmp_img, config='--psm 11')
+    image = Image.open(tmp_img).convert('RGB')
+    # image might be gif and JPG doesn't support transparency
+    #image = image.convert('RGB')
 
-# put possible tccor values into list
-with open(tccor_vars_file) as f :
-    tccor_list = [line.rstrip() for line in f]
+    # enlarge img for better accuracy
+    basewidth = 4000
+    wpercent = (basewidth/float(image.size[0]))
+    hsize = int((float(image.size[1])*float(wpercent)))
+    image = image.resize((basewidth,hsize), Image.ANTIALIAS)
+    # ensure saving image with 400 dpi
+    image.save(tmp_img, dpi=(400,400))
+    # pytesseract work
+    tccordata = pt.image_to_string(tmp_img, config='--psm 11')
 
-# put absolute tccor values into list
-with open(tccor_var_absolute) as tc :
-    tccor_abs_value = [line.rstrip() for line in tc]
+    # put possible tccor values into list
+    with open(tccor_vars_file) as f :
+        tccor_list = [line.rstrip() for line in f]
 
-matches = next((x for x in tccor_abs_value if x in tccordata), False)
-print("Tesseract matched: {}".format(matches))
+    # put absolute tccor values into list
+    with open(tccor_var_absolute) as tc :
+        tccor_abs_value = [line.rstrip() for line in tc]
+
+    matches = next((x for x in tccor_abs_value if x in tccordata), False)
+    print("Tesseract matched: {}".format(matches))
+
+except:
+
+    # look for text in case out of typhoon season
+    results = soup.body.findAll(text="NO ACTIVE TCCOR")
+
+    for i in results:
+        print (i)
